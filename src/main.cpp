0996db1917b769d8e499e6079fe7023c7c085a83
ethanjiang <77793905+ethanjiang1@users.noreply.github.com>
2025-07-21 17:29:11 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,100 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+
+// left motor group
+pros::MotorGroup left_motor_group({-4, -3, -1}, pros::MotorGears::blue);
+// right motor group
+pros::MotorGroup right_motor_group({14, 12, 11}, pros::MotorGears::blue);
+
+// drivetrain settings
+lemlib::Drivetrain drivetrain(&left_motor_group, // left motor group
+                              &right_motor_group, // right motor group
+                              13.5, // 10 inch track width
+                              lemlib::Omniwheel::NEW_275, // using new 2.75" omnis
+                              450, // drivetrain rpm is 450
+                              7 // horizontal drift is 2 (for now)
+                              //wheelbase is 10.5”
+);
+
+// imu
+pros::Imu imu(6);
+// horizontal tracking wheel encoder
+pros::Rotation horizontal_tracking(-9);
+// vertical tracking wheel encoder
+pros::Rotation vertical_tracking(-8);
+// horizontal tracking wheel
+lemlib::TrackingWheel horizontal_tracking_wheel(&horizontal_tracking, 1.966, 3.02);
+// vertical tracking wheel
+lemlib::TrackingWheel vertical_tracking_wheel(&vertical_tracking, 1.966, 0.05);
+
+// odometry settings
+lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel 1, set to null
+                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
+                            &horizontal_tracking_wheel, // horizontal tracking wheel 1
+                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
+                            &imu // inertial sensor
+);
+
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(12, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              155, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(4.25, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              35, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+// create the chassis
+lemlib::Chassis chassis(drivetrain, // drivetrain settings
+                        lateral_controller, // lateral PID settings
+                        angular_controller, // angular PID settings
+                        sensors // odometry sensors
+);
+
+void opcontrol() {
+     // set position to x:0, y:0, heading:0
+    chassis.setPose(0, 0, 0);
+    // move 48" forwards
+	//chassis.turnToHeading(90, 5000);
+    chassis.moveToPose(15, 15, 0, 10000);
+}
+
+// initialize function. Runs on program startup
+void initialize() {
+    pros::lcd::initialize(); // initialize brain screen
+    chassis.calibrate(); // calibrate sensors
+    // print position to brain screen
+    pros::Task screen_task([&]() {
+        while (true) {
+            // print robot location to the brain screen
+            pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
+            pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
+            pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
+            // delay to save resources
+            pros::delay(20);
+        }
+    });
+}
+
+
+
+
+
 /**
  * A callback function for LLEMU's center button.
  *
@@ -14,19 +109,6 @@ void on_center_button() {
 	} else {
 		pros::lcd::clear_line(2);
 	}
-}
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
-void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-
-	pros::lcd::register_btn1_cb(on_center_button);
 }
 
 /**
@@ -58,7 +140,7 @@ void competition_initialize() {}
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
-void autonomous() {}
+//void autonomous() {}
 
 /**
  * Runs the operator control code. This function will be started in its own task
@@ -73,7 +155,7 @@ void autonomous() {}
  * operator control task will be stopped. Re-enabling the robot will restart the
  * task, not resume it from where it left off.
  */
-void opcontrol() {
+/**void opcontrol() {
 	pros::Controller master(pros::E_CONTROLLER_MASTER);
 	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
 	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
@@ -91,4 +173,4 @@ void opcontrol() {
 		right_mg.move(dir + turn);                     // Sets right motor voltage
 		pros::delay(20);                               // Run for 20 ms then update
 	}
-}
+}*/
