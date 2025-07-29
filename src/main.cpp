31a87db1735dca0e45f2251318936d5511e3459d
ethanjiang <77793905+ethanjiang1@users.noreply.github.com>
2025-07-29 19:51:54 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -36,27 +36,39 @@ lemlib::OdomSensors sensors(&vertical_tracking_wheel, // vertical tracking wheel
 );
 
 // lateral PID controller
-lemlib::ControllerSettings lateral_controller(12, // proportional gain (kP)
+lemlib::ControllerSettings lateral_controller(10.5, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              155, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
+                                              45, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
 );
 
 // angular PID controller
 lemlib::ControllerSettings angular_controller(4.25, // proportional gain (kP)
                                               0, // integral gain (kI)
-                                              35, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in degrees
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in degrees
-                                              500, // large error range timeout, in milliseconds
+                                              45, // derivative gain (kD)
+                                              0, // anti windup
+                                              0, // small error range, in inches
+                                              0, // small error range timeout, in milliseconds
+                                              0, // large error range, in inches
+                                              0, // large error range timeout, in milliseconds
                                               0 // maximum acceleration (slew)
+);
+
+// input curve for throttle input during driver control
+lemlib::ExpoDriveCurve throttleCurve(3, // joystick deadband out of 127
+                                     10, // minimum output where drivetrain will move out of 127
+                                     1.019 // expo curve gain
+);
+
+// input curve for steer input during driver control
+lemlib::ExpoDriveCurve steerCurve(3, // joystick deadband out of 127
+                                  10, // minimum output where drivetrain will move out of 127
+                                  1.019 // expo curve gain
 );
 
 // create the chassis
@@ -64,14 +76,19 @@ lemlib::Chassis chassis(drivetrain, // drivetrain settings
                         lateral_controller, // lateral PID settings
                         angular_controller, // angular PID settings
                         sensors // odometry sensors
+                        &throttleCurve, 
+                        &steerCurve
 );
 
 void opcontrol() {
-     // set position to x:0, y:0, heading:0
-    chassis.setPose(0, 0, 0);
+    // set position to x:0, y:0, heading:0
+    // turn to face heading 90 with a very long timeout
+    //chassis.turnToHeading(90, 100000, {.minSpeed = 0, .earlyExitRange = 0.1});
+    //set position to x:0, y:0, heading:0
+    //chassis.setPose(0, 0, 0);
     // move 48" forwards
 	//chassis.turnToHeading(90, 5000);
-    chassis.moveToPose(15, 15, 0, 10000);
+    chassis.moveToPoint(10, 10, 1000);
 }
 
 // initialize function. Runs on program startup
